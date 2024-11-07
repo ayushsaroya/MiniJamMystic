3474869a7fcf47d831616534f130e635ce224b15
Jaedon Bersamin <Birdman.B1324@gmail.com>
2024-11-06 16:19:30 -0800

    JB-initial

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -8,7 +8,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Ayush :)");
+        Debug.Log("Jaedon");
     }
 
     // Update is called once per frame
